0855a0bf815b8ba3216b3d51024271176d10be64
= <dvsocean@icloud.com>
2018-06-10 19:21:11 -0700

    added heroku app

--- src/main/java/jp/ItemServiceClient.java
+++ src/main/java/jp/ItemServiceClient.java
@@ -11,8 +11,13 @@ public class ItemServiceClient {
     public Item getPersonInformation(String baseUrl, int id)
             throws IOException {
 
-        String response =  Request.Get(baseUrl + "/item/" + id).execute().returnContent().asString();
+        String response = Request.Get(baseUrl + "/item/" + id).execute().returnContent().asString();
         Gson gson = new Gson();
         return gson.fromJson(response, Item.class);
+    }
+
+    public void getResponseFromHeroku(String url) throws IOException {
+        String response =  Request.Get(url).execute().returnContent().asString();
+        System.out.println("Output from herokuApp --> " + response);
     }
 }
--- src/test/java/jp/consumer/ItemClientTest.java
+++ src/test/java/jp/consumer/ItemClientTest.java
@@ -19,31 +19,38 @@ public class ItemClientTest {
 
     //Map<String, String> headers = Collections.singletonMap("Content-Type", "application/json");
 
-//    @Rule
-//    public PactProviderRule provider = new PactProviderRule("item_prov", "localhost", 8000, this);
-//
-//    @Pact(provider="item_prov", consumer="item_consumer")
-//    public PactFragment configurationFragment(PactDslWithProvider builder) {
-//        return builder
-//                .given("a an item is present")
-//                .uponReceiving("a request for a specific item")
-//                .path("/item/3")
-//                .method("GET")
-//                .willRespondWith()
-//                //.headers(headers)
-//                .status(200)
-//                .body("{\"description\":\"Cars\",\"completed\":false,\"belongsTo\":\"Evan\"}")
-//                .toFragment();
-//    }
-//
-//    @PactVerification("item_prov")
+    @Rule
+    public PactProviderRule provider = new PactProviderRule("item_prov", "localhost", 8000, this);
+
+    @Pact(provider="item_prov", consumer="item_consumer")
+    public PactFragment configurationFragment(PactDslWithProvider builder) {
+        return builder
+                .given("an item is present")
+                .uponReceiving("a request for a specific item")
+                .path("/item/3")
+                .method("GET")
+                .willRespondWith()
+                //.headers(headers)
+                .status(200)
+                .body("{\"description\":\"Cars\",\"completed\":false,\"belongsTo\":\"Evan\"}")
+                .toFragment();
+    }
+
+    @PactVerification("item_prov")
     @Test
     public void get_person() throws IOException {
-        ItemServiceClient psc = new ItemServiceClient();
-        Item response = psc.getPersonInformation("http://localhost:8000", 3);
+        ItemServiceClient isc = new ItemServiceClient();
+        Item response = isc.getPersonInformation("http://localhost:8000", 3);
         assertThat(response.getBelongsTo(), is("Evan"));
         assertThat(response.isCompleted(), is(false));
         System.out.println("The owner of this item is --> " + response.getBelongsTo());
         System.out.println("The completed status is --> " + response.isCompleted());
     }
+
+    @Test
+    public void simpleReq() throws IOException {
+        ItemServiceClient isc = new ItemServiceClient();
+        isc.getResponseFromHeroku("https://ocean-rest-test.herokuapp.com/itemsObject");
+    }
+
 }
